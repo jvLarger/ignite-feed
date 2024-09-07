14bc089cb6f5ffbeccff4acd035eb6b5b5fc37f3
João Victor <larger13@hotmail.com>
2024-09-06 21:11:59 -0300

    Função para aplaudir comentários

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { Header } from "./components/Header"
-import { Post } from "./components/Post"
-import { Sidebar } from "./components/Sidebar"
+import { Header } from "./components/Header";
+import { Post } from "./components/Post";
+import { Sidebar } from "./components/Sidebar";
 
-import './global.css'
+import "./global.css";
 
-import styles from './App.module.css'
+import styles from "./App.module.css";
 
 const posts = [
   {
@@ -12,59 +12,67 @@ const posts = [
     author: {
       avatarUrl: "https://github.com/jvLarger.png",
       name: "João Victor Larger",
-      role: "Software Developer"
+      role: "Software Developer",
     },
     content: [
-      { type: 'paragraph', content: 'Fala galeraa 👋' },
-      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀', },
-      { type: 'link', content: '👉 <a href="#">jane.design/doctorcare</a>' },
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: '👉 <a href="#">jane.design/doctorcare</a>' },
     ],
-    publishedAt: new Date('2024-09-03 20:00:00'),
+    publishedAt: new Date("2024-09-03 20:00:00"),
   },
   {
     id: 2,
     author: {
       avatarUrl: "https://github.com/diego3g.png",
       name: "Diego",
-      role: "Software Developer"
+      role: "Software Developer",
     },
     content: [
-      { type: 'paragraph', content: 'Fala galeraa 👋' },
-      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀', },
-      { type: 'link', content: '👉 <a href="#">jane.design/doctorcare</a>' },
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: '👉 <a href="#">jane.design/doctorcare</a>' },
     ],
-    publishedAt: new Date('2024-09-02 20:00:00'),
+    publishedAt: new Date("2024-09-02 20:00:00"),
   },
   {
     id: 3,
     author: {
       avatarUrl: "https://github.com/maykbrito.png",
       name: "Mayk Brito",
-      role: "Software Developer"
+      role: "Software Developer",
     },
     content: [
-      { type: 'paragraph', content: 'Fala galeraa 👋' },
-      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀', },
-      { type: 'link', content: '👉 jane.design/doctorcare' },
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "👉 jane.design/doctorcare" },
     ],
-    publishedAt: new Date('2024-09-01 20:00:00'),
-  }
-]
+    publishedAt: new Date("2024-09-01 20:00:00"),
+  },
+];
 
 function App() {
-
   return (
     <>
-
       <Header />
 
       <div className={styles.wrapper}>
-
         <Sidebar />
 
         <main>
-
-          {posts.map(post => {
+          {posts.map((post) => {
             return (
               <Post
                 key={post.id}
@@ -72,15 +80,12 @@ function App() {
                 content={post.content}
                 publishedAt={post.publishedAt}
               />
-            )
+            );
           })}
-
         </main>
-
       </div>
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,44 +1,49 @@
-import { ThumbsUp, Trash } from 'phosphor-react';
-import styles from './Comment.module.css'
-import { Avatar } from './Avatar';
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+import { Avatar } from "./Avatar";
+import { useState } from "react";
 
-export function Comment({comment, onDeleteComment}) {
+export function Comment({ comment, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
 
-    function handleDeleteComment() {
-        onDeleteComment(comment);
-    }
+  function handleDeleteComment() {
+    onDeleteComment(comment);
+  }
 
-    return (
-        <div className={styles.comment}>
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1);
+  }
 
-            <Avatar
-                hasBorder={false}
-                src='https://github.com/jvLarger.png'
-            />
+  return (
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src="https://github.com/jvLarger.png" />
 
-            <div className={styles.commentBox}>
-                <div className={styles.commentContent}>
-                    <header>
-                        <div className={styles.authorAndTime}>
-                            <strong>João Victor Larger</strong>
-                            <time title='1 de setembro às 17:39h' dateTime="2024-09-01 17:39:00">Cerca de 1h atrás</time>
-                        </div>
-                        <button onClick={handleDeleteComment} title="Deletar Comentário">
-                            <Trash size={24} />
-                        </button>
-                    </header>
-                    <p>
-                        {comment}
-                    </p>
-                </div>
-                <footer>
-                    <button>
-                        <ThumbsUp />
-                        Aplaudir
-                        <span>20</span>
-                    </button>
-                </footer>
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>João Victor Larger</strong>
+              <time
+                title="1 de setembro às 17:39h"
+                dateTime="2024-09-01 17:39:00"
+              >
+                Cerca de 1h atrás
+              </time>
             </div>
+            <button onClick={handleDeleteComment} title="Deletar Comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>{comment}</p>
         </div>
-    );
+        <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp />
+            Aplaudir
+            <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
 }
